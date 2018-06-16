1a825bfa85351b9468c1a0938df526e7f15f674c
Jiwon Bae <jwb8705@gmail.com>
2018-06-16 23:47:56 +0900

    chore: [Setup] - project set up

---
+++ src/main/java/my_daily_feed/MyDailyFeedApplication.java
@@ -0,0 +1,12 @@
+package my_daily_feed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDailyFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDailyFeedApplication.class, args);
+    }
+}
