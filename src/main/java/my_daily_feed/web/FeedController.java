e6ce7156e3a7ac5c441b4ca9c7d97521c1736f02
Jiwon Bae <jwb8705@gmail.com>
2018-08-17 01:26:33 +0900

    feat: [deploy-test] Tries to resolve build error

--- src/main/java/my_daily_feed/web/FeedController.java
+++ src/main/java/my_daily_feed/web/FeedController.java
@@ -1,14 +1,22 @@
 package my_daily_feed.web;
 
+import my_daily_feed.domain.Feed;
 import my_daily_feed.service.FeedService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 @RestController
+@RequestMapping("/api/feeds")
 public class FeedController {
     @Resource(name= "feedService")
     private FeedService feedService;
 
+    @PostMapping("")
+    public Feed create(){
+        return null;
+    }
 
 }
